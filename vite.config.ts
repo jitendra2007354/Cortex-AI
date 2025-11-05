c4d73a4f450c6a50d7ce554b9dcdd52783998d72
jitendra2007354 <jitendrasinghchauhan3008@gmail.com>
2025-11-05 22:41:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/Cortex-AI/', // ✅ Required for GitHub Pages ✅
+    main: '/Cortex-AI/', // ✅ Required for GitHub Pages ✅
 
     server: {
       port: 3000,
